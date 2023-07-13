f646e692020d651255f4b6c7385ad3cb153f0ece
qiualu <1101938064@qq.com>
2023-07-13 10:23:47 +0800

    同步插件

---
+++ serpent/plugins/SerpentDNFGamePlugin/plugin.py
@@ -0,0 +1,35 @@
+import offshoot
+
+from serpent.Editlibrary.yl_offshoot.base import executable_hook
+from serpent.Editlibrary.yl_offshoot.plugin import Plugin
+
+
+class SerpentDNFGamePlugin(Plugin):
+    name = "SerpentDNFGamePlugin"
+    version = "0.1.0"
+
+    libraries = []
+
+    files = [
+        {"path": "serpent_DNF_game.py", "pluggable": "Game"}
+    ]
+
+    config = {
+        "fps": 2
+    }
+
+    @classmethod
+    def on_install(cls):
+        print("\n\n%s was installed successfully!" % cls.__name__)
+
+    @classmethod
+    def on_uninstall(cls):
+        print("\n\n%s was uninstalled successfully!" % cls.__name__)
+
+def plugin_main(command):
+    print("plugin_main 插件文件开始运行")
+    executable_hook(SerpentDNFGamePlugin, command)
+
+
+if __name__ == "__main__":
+    offshoot.executable_hook(SerpentDNFGamePlugin)
---
+++ 函数案例说明/offshoot说明/plugins/SerpentMLAGamePlugin/plugin.py
@@ -0,0 +1,31 @@
+import offshoot,sys
+
+
+class SerpentGamePlugin(offshoot.Plugin):
+    name = "SerpentGamePlugin"
+    version = "0.1.0"
+
+    libraries = []
+
+    files = [
+        {"path": "serpent_game.py", "pluggable": "Game"}
+    ]
+
+    config = {
+        "fps": 2
+    }
+
+    @classmethod
+    def on_install(cls):
+        print("\n\n%s was installed successfully!" % cls.__name__)
+
+    @classmethod
+    def on_uninstall(cls):
+        print("\n\n%s was uninstalled successfully!" % cls.__name__)
+
+
+if __name__ == "__main__":
+
+    print("plugin main ---")
+    print(sys.argv)
+    offshoot.executable_hook(SerpentGamePlugin)
